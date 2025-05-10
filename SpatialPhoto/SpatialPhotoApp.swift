3c8224b9c26bddb70bd5855e3f0db882f7916302
Kazuya Ueoka <info@fromkk.info>
2025-05-10 17:08:50 +0900

    Initial Commit

---
+++ SpatialPhoto/SpatialPhotoApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialPhotoApp.swift
+//  SpatialPhoto
+//
+//  Created by Kazuya Ueoka on 2025/05/10.
+//
+
+import SwiftUI
+
+@main
+struct SpatialPhotoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
